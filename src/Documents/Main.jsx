a339315d1180c0854f611a205113a2720ca68290
SOWBURNIKA-NALLAMUTHU <727722euit180@skcet.ac.in>
2023-11-20 16:59:15 +0530

    form components

---
+++ src/Documents/Main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <b>
+        <h1 style={{ color: "purple" }}>
+          <center>"FAV SHOP"</center>
+        </h1>
+      </b>
+      <br></br>
+      <h3 style={{ color: "midnightblue" }}>
+        <center>From Heart to Hand: Where Gifts Become Masterpieces</center>
+      </h3>
+      <h3 style={{ color: "midnightblue" }}>
+        <center>From Heart to Hand: Where Gifts Become Masterpieces</center>
+      </h3>
+      <h3 style={{ color: "midnightblue" }}>
+        <center>From Heart to Hand: Where Gifts Become Masterpieces</center>
+      </h3>
+      <h3 style={{ color: "midnightblue" }}>
+        <center>From Heart to Hand: Where Gifts Become Masterpieces</center>
+      </h3>
+      <h3 style={{ color: "midnightblue" }}>
+        <center>From Heart to Hand: Where Gifts Become Masterpieces</center>
+      </h3>
+      <h3 style={{ color: "midnightblue" }}>
+        <center>From Heart to Hand: Where Gifts Become Masterpieces</center>
+      </h3>
+    </div>
+  );
+};
+
+export default Main;
